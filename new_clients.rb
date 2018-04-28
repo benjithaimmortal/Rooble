506d20c9b472bc91616439ce9301bf5e9e40eea8
Benjamin Kostenbader <benjamin.kostenbader@gmail.com>
2018-04-29 08:09:03 +0900

    updating the block more

--- new_clients.rb
+++ new_clients.rb
@@ -6,6 +6,7 @@ end
 
 
 class Client
+	include Balances
 	def initialize (client_name, investment_dollars, balance_cents, account_number=nil)
 		@client_name = client_name
 		@account_number = account_number = rand(10000..99999) if @account_number.nil?
@@ -14,20 +15,23 @@ class Client
 
 		balance_cents.each do |k,v|
 			@balance_cents = v * @investment_dollars
-			if k == savings || checking || money_market then [@account = k] else raise NotImplementedError end
+			if k == "savings" || k == "checking" || k == "money_market" then account = lambda { self.k } else raise NotImplementedError end
+			account
 			puts "Hi it's $#{investment_dollars} from the block, but I couldn't transfer to a #{k} class :("
-			self.savings.show_balance
 		end
 	end
 
 	def savings
 		account = Savings.new(@client_name, @account_number, @investment_dollars)
+		account.show_balance
 	end
 	def checking
 		account = Checking.new(@client_name, @account_number, @investment_dollars)
+		account.show_balance
 	end
 	def money_market
 		account = MoneyMarket.new(@client_name, @account_number, @investment_dollars)
+		account.show_balance
 	end
 end
 
@@ -46,21 +50,18 @@ end
 class Checking < Account
 	def initialize(client_name, account_number, balance_cents, transaction_limit = nil)
 		super(client_name, account_number, balance_cents, transaction_limit)
-		puts "Initializing #{self.class.to_s}"
 	end
 end
 
 class Savings < Account
 	def initialize(client_name, account_number, balance_cents, transaction_limit = 3)
 		super(client_name, account_number, balance_cents, transaction_limit)
-		puts "Initializing #{self.class.to_s}"
 	end
 end
 
 class MoneyMarket < Account
 	def initialize(client_name, account_number, balance_cents, transaction_limit = nil)
 		super(client_name, account_number, balance_cents, transaction_limit)
-		puts "Initializing #{self.class.to_s}"
 	end
 end
 
